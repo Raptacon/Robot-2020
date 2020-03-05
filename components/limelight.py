6c880e9d4e9d1d59068374aa9458ba19cd09f044
DrKittyKat <dr.kittykat77@gmail.com>
2020-03-04 23:48:25 -0700

    barebones limelight.py component:
    • Created component
    • Added basic setup
    • Created functions to fill in

---
+++ components/limelight.py
@@ -0,0 +1,23 @@
+"""
+Manager to more easily read the values 
+from the limelight and more easily write 
+to for pipeline changes
+"""
+
+class Limelight:
+    """
+    Class for access and assignment of values. 
+    This is a low level component
+    """
+
+    def setup(self):
+        """
+        Sets up the limelight access. Since it is
+        a "plug and play" device, there is no need
+        for any actual component creation
+        """
+
+        pass
+
+    def execute(self):
+        pass
