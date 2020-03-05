a86b88f65a1ed0bc8db9a45cfd2d838e3605b2d2
DrKittyKat <dr.kittykat77@gmail.com>
2020-03-05 07:32:49 -0700

    Filled out the "rest" of the functions

--- components/limelight.py
+++ components/limelight.py
@@ -7,10 +7,20 @@ from networktables import NetworkTables
 from enum import Enum
 
 class LimelightCamMode(Enum):
+    """
+    Limelight defined camera modes that
+    can be set through the network tables.
+    kDriverMode provides a human useable view,
+    kProcessingMode is for computer processing
+    """
     kDriverMode = 1
     kProcessingMode = 0
 
 class LimelightLightMode(Enum):
+    """
+    limelight defined light modes that can
+    be set through the network tables
+    """
     kDefault = 0
     kOff = 1
     kBlink = 2
@@ -32,31 +42,71 @@ class Limelight:
         self.limelightTable = NetworkTables.getTable("limelight")
 
     def _checkForObject(self):
-        assert self.limelightTable.getNumber("tx") == 1, "there is no object in view, cannot get offset" #this is pretty weird for me - just let me know or fix it if it is overkill
+        """
+        throws an error if there is not an object
+        in view. This leaves it up to the calling
+        object on what to do.
+        """
+        assert self.limelightTable.getNumber("tv") == 1, "there is no object in view, cannot get offset" #this is pretty weird for me - just let me know or fix it if it is overkill
 
     def getXOffset(self):
         """
-        gets from the network tables how far off the
-        crosshair is from the sensed targed. throws an
-        error if there is no target in view
+        gets, from the network tables, how far off, in the
+        horizontal direction, the crosshair is from the sensed 
+        targed. throws an error if there is no target in view
         """
         _checkForObject(self)
         return self.limelightTable.getNumber("tx")
 
     def getYOffset(self):
-        pass
+        """
+        gets, from the network tables, how far off, in the
+        vertical direction, the crosshair is from the sensed 
+        targed. throws an error if there is no target in view
+        """
+        _checkForObject(self)
+        return self.limelightTable.getNumber("ty")
 
     def getSize(self):
-        pass
+        """
+        returns the area of a sensed target, from 0 to 100.
+        This is a percent of the image.
+        """
+        _checkForObject(self)
+        return self.limelightTable.getNumber("ta")
 
-    def setPipeline(self):
-        pass
+    def setPipeline(self, pipeline: int):
+        """
+        Sets the pipeline for the limelight to target. 
+        returns the previous pipeline
+        """
+        prevPipeline = self.limelightTable.getNumber("pipeline")
+        self.limelightTable.putNumber("pipeline", pipeline)
+        return prevPipeline
 
     def setCameraMode(self, mode: LimelightCamMode):
-        pass
+        """
+        set the limelight to either driver mode or
+        processing mode, using the enumerated class
+        return the previous mode
+        """
+        prevMode = self.limelightTable.getNumber("camMode")
+        self.limelightTable.putNumber("camMode", mode)
+        return prevMode
 
     def setLEDMode(self, mode: LimelightLightMode):
-        pass
+        """
+        sets the limelight led to one of the enumerated values from
+        class LimelightLightMode. Make sure to use sparingly
+        returns the previous mode
+        """
+        prevLED = self.limelightTable.getNumber("ledMode")
+        self.limelightTable.putNumber("ledMode")
+        return prevLED
 
     def execute(self):
+        """
+        limelight functions to run every frame ostensibly, we
+        won't use this at all
+        """
         pass
