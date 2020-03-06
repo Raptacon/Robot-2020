b933e10219da62f2cb8b094d91c30ad49239a5a8
DrKittyKat <dr.kittykat77@gmail.com>
2020-03-06 15:13:27 -0700

    imported logging, added exception handler, functionality to execute

--- components/align.py
+++ components/align.py
@@ -5,6 +5,7 @@ to align the bot to a target
 from components.driveTrain import DriveTrain
 from components.limelight import Limelight, LimelightCamMode, LimelightLightMode
 from enum import Enum, auto
+import logging
 
 class AlignStates(auto):
     kNone = auto()
@@ -28,6 +29,15 @@ class Align():
         self.currentAligningState = AlignStates.kAliginingXY
 
     def execute(self):
-        """if self.currentAligningState == AlignStates.ali
-            offset = Limelight.getXOffset()
-            self.drivetrain.setArcade(0, offset)"""
+        try:
+            xoffset = Limelight.getXOffset()
+        except AssertionError as error:
+            logging.error("Alignment failed due to error: %s"% error)
+
+        if self.currentAligningState == AlignStates.kAligningX:
+            self.drivetrain.setArcade(0, xoffset)
+        elif self.currentAligningState == AlignStates.kAliginingXY:
+            self.drivetrain.setArcade(0, xoffset)
+            logging.info("Aligning to Y not yet implemented, skipping")
+        elif self.currentAligningState == AlignStates.kAliginingY:
+            logging.info("Aligning to Y not yet implemented, skipping")
