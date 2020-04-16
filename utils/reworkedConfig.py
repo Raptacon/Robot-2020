50a78c7998266eda21fffa5a5f4755933c10d488
cannedlock <zwinter03@gmail.com>
2020-04-16 04:47:58 -0600

    more working stuff...

--- utils/reworkedConfig.py
+++ utils/reworkedConfig.py
@@ -6,6 +6,93 @@ from chardet import detect
 from string import ascii_lowercase
 from pathlib import Path
 
+class ConfigMapper:
+
+    def __init__(self, robot, fileName: str, specifiedConfig = None):
+        #self.robot = robot
+        self.configFileName = fileName
+        loadedFile = self.__loadFile(fileName)
+        config, configName = self.__getConfig(loadedFile, requestedConfig = specifiedConfig)
+
+        self.configName = configName
+        self.configCompat = config['compatibility']
+
+        self.testStuff(config)
+
+    def __loadFile(self, fileName):
+        configFile = dirname(__file__) + os.path.sep + '..' + os.path.sep + fileName
+        with open(configFile) as file:
+            loadedFile = json.load(file)
+        return loadedFile
+
+    def __getConfig(self, configFile, requestedConfig = None):
+        """
+        Method takes a file name as the config file and finds a requested config for the robot, changing it appropriatly.
+        If no config is requested, use the default config listed in the file.
+        """
+
+        config = {}
+        defaultConfig = configFile['default']
+        robotConfigs = []
+
+        # Create a list of all robot configs (excluding subconfigs)
+        # NOTE: For a config to be defined as a robot config, a 'compatibility' key must be declared
+        for key in configFile:
+            if 'compatibility' in configFile[key]:
+                robotConfigs.append(key)
+
+        # If no requested config, use default
+        if requestedConfig == None:
+            log.warning("No config requested. Using default: %s" %(defaultConfig))
+            config.update(configFile[defaultConfig])
+            configName = defaultConfig
+
+        # Find requested config in list of config names, then use
+        elif requestedConfig in robotConfigs:
+            log.info("Requsted config '%s' found in %s. Loading..." %(requestedConfig, self.configFileName))
+            config.update(configFile[requestedConfig])
+            configName = requestedConfig
+
+        # If requested config isn't found, raise error and exit
+        else:
+            raise AttributeError(
+                """
+                Requested config '%s' could not be found. Current configs: %s.
+                Is there a 'compatibility' key in '%s'?
+                """ %(requestedConfig, robotConfigs, requestedConfig)
+            )
+
+        # Return filtered config and name of config
+        return config, configName
+
+    def testStuff(self, config):
+        for group, data in config.items():
+            if isinstance(data, list):
+                for dictionary in data:
+                    if 'factory' in dictionary:
+                        factory = dictionary['factory']
+                    if 'subsystem' in dictionary:
+                        subsystem_name = dictionary.pop('subsystem')
+                        subsystem = dictionary
+                        print(f"Group Name: {group}, Subsystem Name: {subsystem_name}, Factory: {factory}, Group: {subsystem}")
+    
+    def almostThere(self, robot, groupName, subsystemName, group, factory):
+        pass
+
+    def checkCompatibility(self, compatString):
+        """
+        Checks compatibility of the component based on the compatString and the compatibility key in the config.
+        """
+
+        compatString = [x.lower() for x in compatString]
+        root = [self.configCompat] # This is the compatibility of the loaded config
+        if "all" in root or "all" in compatString:
+            return True
+        for string in root:
+            if string in compatString:
+                return True
+        return False
+
 def findConfig(use_encoding = True, strict = False):
     """
     Sets the config to be used one the robot. To manually set a config, run 'echo [config name] > RobotConfig' on the robot.
@@ -72,74 +159,5 @@ def findConfig(use_encoding = True, strict = False):
 
     return configString
 
-class ConfigMapper:
-
-    def __init__(self, robot, fileName: str, specifiedConfig = None):
-        self.configFileName = fileName
-        loadedFile = self.__loadFile(fileName)
-        config, configName = self.__getConfig(loadedFile, requestedConfig = specifiedConfig)
-        self.configName = configName
-        self.configCompat = config['compatibility']
-
-    def __loadFile(self, fileName):
-        configFile = dirname(__file__) + os.path.sep + '..' + os.path.sep + fileName
-        with open(configFile) as file:
-            loadedFile = json.load(file)
-        return loadedFile
-
-    def __getConfig(self, configFile, requestedConfig = None):
-        """
-        Method takes a file name as the config file and finds a requested config for the robot, changing it appropriatly.
-        If no config is requested, use the default config listed in the file.
-        """
-
-        config = {}
-        defaultConfig = configFile['default']
-        robotConfigs = []
-
-        # Create a list of all robot configs (excluding subconfigs)
-        # NOTE: For a config to be defined as a robot config, a 'compatibility' key must be declared
-        for key in configFile:
-            if 'compatibility' in configFile[key]:
-                robotConfigs.append(key)
-
-        # If no requested config, use default
-        if requestedConfig == None:
-            log.warning("No config requested. Using default: %s" %(defaultConfig))
-            config.update(configFile[defaultConfig])
-            configName = defaultConfig
-
-        # Find requested config in list of config names, then use
-        elif requestedConfig in robotConfigs:
-            log.info("Requsted config '%s' found in %s. Loading..." %(requestedConfig, self.configFileName))
-            config.update(configFile[requestedConfig])
-            configName = requestedConfig
-
-        # If requested config isn't found, raise error and exit
-        else:
-            raise AttributeError(
-                """
-                Requested config '%s' could not be found. Current configs: %s.
-                Is there a 'compatibility' key in '%s'?
-                """ %(requestedConfig, robotConfigs, requestedConfig)
-            )
-
-        # Return filtered config and name of config
-        return config, configName
-
-    def generateFactoryObjects(self, robot, config):
-        
-
-    def checkCompatibility(self, compatString):
-        """
-        Checks compatibility of the component based on the compatString and the compatibility key in the config.
-        """
-
-        compatString = [x.lower() for x in compatString]
-        root = [self.configCompat] # This is the compatibility of the loaded config
-        if "all" in root or "all" in compatString:
-            return True
-        for string in root:
-            if string in compatString:
-                return True
-        return False
+if __name__ == '__main__':
+    configMapper = ConfigMapper(None, 'robot.json')
