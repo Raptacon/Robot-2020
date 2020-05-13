5cd8af8564020a55db951c02a54898e91ee0d3a5
cannedlock <zwinter03@gmail.com>
2020-05-13 07:30:37 -0600

    added a lot to FileHandler

--- utils/reworkedConfig.py
+++ utils/reworkedConfig.py
@@ -18,7 +18,7 @@ else:
 
 class FileHandler:
     """
-    Various helper methods for finding and loading files.
+    Various helper methods for finding and loading files/folders.
     """
 
     @staticmethod
@@ -32,7 +32,7 @@ class FileHandler:
         return loadedFile
 
     @staticmethod
-    def directory(name):
+    def file_directory(name) -> str:
         """
         Attempt to get the directory of a requested file.
         """
@@ -43,6 +43,44 @@ class FileHandler:
             del dirs
             if name in files:
                 return os.path.join(root, name)
+
+    @staticmethod
+    def folder_directory(name) -> str:
+        """
+        Attempt to get the directory of a requested folder.
+        """
+
+        path = os.getcwd()
+
+        for root, dirs, files in os.walk(path):
+            del files
+            if name in dirs:
+                return os.path.join(root, name)
+
+    @staticmethod
+    def get_all_files(folder, extentions = False) -> list:
+        """
+        Lists the names of all the files living within a folder.
+        NOTE this function automatically removes `__init__.py`
+        from the list.
+        """
+
+        path = FileHandler.folder_directory(folder)
+
+        files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+
+        filtered_files = []
+
+        for file in files:
+            if file.startswith('_'):
+                continue
+            if not extentions:
+                split_file = file.split('.')
+                filtered_files.append(split_file[0])
+            else:
+                filtered_files.append(file)
+
+        return filtered_files
 
 class ConfigurationManager(FileHandler):
     """
@@ -60,17 +98,17 @@ class ConfigurationManager(FileHandler):
         #       portion of this file.
         #
 
-        setup_data = self.load(self.directory('setup.json'))
+        setup_data = self.load(self.file_directory('setup.json'))
         default_config, requirements = self.__getSetupInfo(setup_data)
-        factory_data = self.load(self.directory('.factories.json'))
+        factory_data = self.load(self.file_directory('.factories.json'))
 
         try:
-            _dir = self.directory(config)
+            _dir = self.file_directory(config)
             loadedFile = self.load(_dir)
             self.configName = config
         except TypeError:
             log.warning("No config requested. Using default config: %s" %(default_config))
-            _dir = self.directory(default_config)
+            _dir = self.file_directory(default_config)
             loadedFile = self.load(_dir)
             self.configName = default_config
 
