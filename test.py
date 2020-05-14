3d3d1777555b904c7fe1d0817a2b794feb916be8
cannedlock <zwinter03@gmail.com>
2020-05-14 13:02:21 -0600

    even more testing

--- test.py
+++ test.py
@@ -4,5 +4,6 @@ from time import sleep
 os.popen('git checkout master')
 
 if os.popen('git branch --show-current').readline().strip() == 'master':
+    print(os.popen('git branch --show-current').readline().strip())
     sleep(2)
     os.popen('git checkout RoboWindow')
