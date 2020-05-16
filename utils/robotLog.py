d48f63d4cb13167efdcd7606c1c9c2f972a564af
cannedlock <zwinter03@gmail.com>
2020-05-16 17:10:51 -0600

    more specific logs

--- utils/robotLog.py
+++ utils/robotLog.py
@@ -11,10 +11,12 @@ def __getRunInfo():
     commit_hash = os.popen('git rev-parse HEAD').readline().strip()
     branch = os.popen('git branch --show-current')
     branch_name = branch.readline().strip()
+    git_type = 'branch'
     runtime_version = branch_name
     if not branch_name:
         tag = os.popen('git describe --tags')
         tag_name = tag.readline().strip()
+        git_type = 'version'
         runtime_version = tag_name
 
     # Determine simulation status
@@ -28,9 +30,9 @@ def __getRunInfo():
     date = currentDT.strftime("%a, %b %d, %Y")
     time = currentDT.strftime("%I:%M:%S %p")
 
-    return is_simulation, runtime_version, commit_hash, date, time
+    return is_simulation, runtime_version, git_type, commit_hash, date, time
 
-def __createEntry(is_sim, version, commit, date, time):
+def __createEntry(is_sim, version, git_type, commit, date, time):
     """
     Creates a logged entry based on runtime information.
     Requires info arguments.
@@ -43,7 +45,7 @@ def __createEntry(is_sim, version, commit, date, time):
         run_type = "Deploy"
     with open(log_dir, 'a') as txt_file:
         txt_file.write(
-            run_type + " with version/branch '" + version + "' (commit hash " + commit + ") " + "on " + date + " at " + time + '\n'
+            f"{run_type} with {git_type} '{version}' (commit hash {commit}) on {date} at {time} \n"
         )
 
 def log(deploys = True, simulations = False):
@@ -55,9 +57,9 @@ def log(deploys = True, simulations = False):
     :param simulations: If set to True, write simulations to log file.
     """
 
-    is_simulation, version, commit, date, time = __getRunInfo()
+    is_simulation, version, git_type, commit, date, time = __getRunInfo()
 
     if simulations and is_simulation:
-        __createEntry(is_simulation, version, commit, date, time)
+        __createEntry(is_simulation, version, git_type, commit, date, time)
     elif deploys and not is_simulation:
-        __createEntry(is_simulation, version, commit, date, time)
+        __createEntry(is_simulation, version, git_type, commit, date, time)
