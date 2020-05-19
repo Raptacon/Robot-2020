3aec4d562e09f2fdba547edd1dca66ca81127a0e
cannedlock <zwinter03@gmail.com>
2020-05-19 12:46:33 -0600

    simplified findConfig

--- utils/reworkedConfig.py
+++ utils/reworkedConfig.py
@@ -101,24 +101,20 @@ class ConfigurationManager(FileHandler):
             try:
                 with open(configDir, 'rb') as file:
                     raw_data = file.readline().strip()
-                log.info("Config found in %s" %(configDir))
+                log.info(f"Config found in {configDir}")
                 encoding_type = ((detect(raw_data))['encoding']).lower()
                 with open(configDir, 'r', encoding = encoding_type) as file:
                     configString = file.readline().strip()
-            except:
-                log.warning(f"{configDir} could not be found.")
-                configString = None
+            except FileNotFoundError:
+                log.error(f"{configDir} could not be found.")
+                configString = default_config
             finally:
                 return configString
 
         config = findConfig()
 
-        if config:
-            log.info("Using config '%s'" %(config))
-            loadedConfig = self.load(config)
-        else:
-            log.warning("No config requested. Using default config: %s" %(default_config))
-            loadedConfig = self.load(default_config)
+        log.info(f"Using config '{config}'")
+        loadedConfig = self.load(config)
 
         self.compatibility = loadedConfig['compatibility']
         subsystems = loadedConfig['subsystems']
@@ -126,7 +122,7 @@ class ConfigurationManager(FileHandler):
 
         log.info(f"Creating {len(subsystems)} subsystems")
 
-        # Generate robot objects from factoriesS
+        # Generate robot objects from factories
         for subsystem_name, subsystem_data in subsystems.items():
             for group_name, group_info in subsystem_data.items():
                 factory = getattr(import_module(factory_data[group_name]['file']), factory_data[group_name]['func'])
