04423d09ad258cb8404c48edf4d41ae0ac64541e
cannedlock <zwinter03@gmail.com>
2020-05-23 16:14:16 -0600

    made more pythonic

--- utils/reworkedConfig.py
+++ utils/reworkedConfig.py
@@ -24,10 +24,10 @@ class ConfigurationManager(FileHandler):
             try:
                 with open(configDir, 'rb') as rf:
                     raw_data = rf.readline().strip()
-                log.info(f"Config found in {configDir}")
                 encoding_type = ((detect(raw_data))['encoding']).lower()
-                with open(configDir, 'r', encoding = encoding_type) as file:
+                with open(configDir, 'r', encoding=encoding_type) as file:
                     configString = file.readline().strip()
+                log.info(f"Config found in {configDir}")
             except FileNotFoundError:
                 log.error(f"{configDir} could not be found.")
                 configString = default_config
@@ -46,7 +46,9 @@ class ConfigurationManager(FileHandler):
         log.info(f"Creating {len(subsystems)} subsystem(s)")
         for subsystem_name, subsystem_data in subsystems.items():
             for group_name, group_info in subsystem_data.items():
-                factory = getattr(import_module(factory_data[group_name]['file']), factory_data[group_name]['func'])
+                _file = import_module(factory_data[group_name]['file'])
+                _func = factory_data[group_name]['func']
+                factory = getattr(_file, _func)
                 items = {key:factory(descp) for key, descp in group_info.items()}
                 groupName_subsystemName = '_'.join([group_name, subsystem_name])
                 setattr(robot, groupName_subsystemName, items)
