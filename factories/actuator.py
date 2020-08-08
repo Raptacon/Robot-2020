20b7849405aa23afff83c86ea87b31e6e2d0bc4c
cannedlock <zwinter03@gmail.com>
2020-08-08 03:04:03 -0600

    small changes

---
+++ factories/actuator.py
@@ -0,0 +1,24 @@
+
+def motorFactory(descp):
+    pass
+
+def gyroFactory(descp):
+    pass
+
+def breaksensorFactory(descp):
+    pass
+
+def compressorFactory(descp):
+    pass
+
+def solenoidFactory(descp):
+    pass
+
+group_mappings = {
+    'motors': motorFactory,
+    'gyros': gyroFactory,
+    'digitalInput': breaksensorFactory,
+    'compressors': compressorFactory,
+    'solenoids': solenoidFactory
+}
+
